4654259f0b30642139e9f559bc574f48341412b0
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-07 19:42:18 -0300

    fix: user route

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-app.use('/api.audibert', userRoutes);
+app.use('/user', userRoutes);
 
 app.use('*', (req, res) => {
   res.status(404).json({
