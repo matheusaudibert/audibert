3bcb747263714b2862f58e72008206ad155eff23
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-07 06:15:01 -0300

    fix: .env.examples

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 module.exports = {
   PORT: 3000,
-  GUILD_ID: '1112920281367973900',
+  GUILD_ID: process.env.GUILD_ID,
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
   DISCORD_AUTH: process.env.DISCORD_AUTH,
   DEFAULT_IMAGE: "https://www.pngarts.com/files/3/Logo-PNG-Image-Background.png"
