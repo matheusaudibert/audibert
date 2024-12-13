555fe10650a7f5c2d988f16f9a40393905d9ac8a
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-13 02:30:39 -0300

    fix: token

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 module.exports = {
+  
   PORT: 3000,
   GUILD_ID: process.env.GUILD_ID,
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
