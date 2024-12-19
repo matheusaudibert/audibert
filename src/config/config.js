fc934d802b1ba8d1bd60c0cb330c4171184af053
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-19 15:03:14 -0300

    feature: webhook

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,6 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 module.exports = {
-  PORT: 3000,
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
   DISCORD_AUTH: process.env.DISCORD_AUTH,
   DEFAULT_IMAGE: "https://i.ibb.co/kqQ14Gn/server-logo-4.png"
