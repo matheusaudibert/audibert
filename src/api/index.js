f2be4d22bd0e44284dceef193cbf1bfed6db7fb0
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-19 14:38:18 -0300

    fix: api

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,8 @@
-const app = require('../src/app');
+const app = require('../app');
+const client = require('../services/discordClient');
+
+client.once('ready', () => {
+  console.log('Bot is online!');
+});
+
 module.exports = app;
