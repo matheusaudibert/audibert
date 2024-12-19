95f896ce40bec588db394b661c0e649795366f4d
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-19 15:15:56 -0300

    fix: config

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,15 @@
 const app = require('../app');
+const client = require('../services/discordClient');
+const config = require('../config/config');
+
+client.once('ready', () => {
+  console.log('Bot is online!');
+});
+
+client.login(config.DISCORD_TOKEN);
+
+app.listen(3000, () => {
+  console.log(`API is running on port 3000`);
+});
 
 module.exports = app;
