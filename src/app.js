1e6f4ddf8ba514c51b9d975ab77c74ee0c048512
Matheus Audibert <matheusaudibert2019@outlook.com>
2024-12-26 20:59:10 -0300

    fix: error json

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use('/user', userRoutes);
 
 app.listen(3000, () => {
