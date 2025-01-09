72b6361353334d3fec8964500ad4180619feb204
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-01-08 21:29:44 -0300

    fix: ?????????

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
       process.env.DISCORD_AUTH_3
     ];
     const randomAuthToken = authTokens[Math.floor(Math.random() * authTokens.length)];
-    
+  
     fetch(`https://canary.discord.com/api/v10/users/${USER_ID}/profile`, {
       method: "GET",
       headers: { "authorization": randomAuthToken }
