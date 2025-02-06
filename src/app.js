519fc37d6f078e0190884eca4231d4b1e3f63812
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-02-06 17:51:18 -0300

    client init 6

--- src/app.js
+++ src/app.js
@@ -6,18 +6,9 @@ const userRoutes = require("./routes/userRoutes");
 const guildRoutes = require("./routes/guildRoutes");
 const activityRoutes = require("./routes/quickRoutes/activityRoutes");
 const guildsRoutes = require("./routes/guildsRoutes");
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildMembers,
-  ],
-});
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = config.PORT;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
@@ -31,29 +22,17 @@ app.use("/guild", guildRoutes);
 app.use("/guilds", guildsRoutes);
 app.use("/activity", activityRoutes);
 
-client.once("ready", () => {
-  client.user.setPresence({
-    activities: [
-      {
-        name: "api.audibert.rest",
-        type: ActivityType.Playing,
-      },
-    ],
-  });
+app.listen(PORT, () => {
+  console.log(`API running in port ${PORT}`);
+});
 
-  console.log("Bot is online and ready!");
-
-  app.listen(PORT, () => {
-    console.log(`API running on port ${PORT}`);
+app.use("*", (req, res) => {
+  res.status(404).json({
+    error: {
+      code: "404_not_found",
+      message: "Route does not exist",
+    },
   });
 });
 
-client.on("error", (error) => {
-  console.error("Error in client:", error);
-});
-
-client.login(config.DISCORD_TOKEN).catch((error) => {
-  console.error("Error logging in:", error);
-});
-
-module.exports = { app, client };
+module.exports = app;
