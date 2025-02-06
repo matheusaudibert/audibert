047bcd0bc2d52cfac4c8dad1882fc699d60251cb
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-02-06 17:32:56 -0300

    client init 3

--- src/services/discordClient.js
+++ src/services/discordClient.js
@@ -20,7 +20,6 @@ client.once("ready", () => {
   });
   console.log("Bot is online and ready!");
 
-  // Agora que o bot está pronto, podemos iniciar o servidor Express
   require("../app").listen(config.PORT, () => {
     console.log(`API is running on port ${config.PORT}`);
   });
