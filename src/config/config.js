cccd2afff5714ad626c82bc84ee9ba9f569d0a38
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-02-06 22:04:09 -0300

    images

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,6 @@ require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 module.exports = {
   PORT: process.env.PORT || 3000,
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
-  DEFAULT_ACTIVITY_IMAGE: "https://ibb.co/yF7QFQY6",
-  DEFAULT_SERVER_IMAGE: "https://ibb.co/WpG4T1M",
+  DEFAULT_ACTIVITY_IMAGE: "https://i.ibb.co/1GjmGmdr/a.png",
+  DEFAULT_SERVER_IMAGE: "https://i.ibb.co/Hfy65cGT/47700d2e75da.png",
 };
